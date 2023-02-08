bc4391b599cdffdc03b7e18fe2eea4ad604bd790
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2023-02-08 18:36:35 +0100

    Begin setup of dom interaction with API logic

--- src/index.js
+++ src/index.js
@@ -7,6 +7,33 @@ const cityInput = document.querySelector('input[type=text]');
 const unitSelector = document.querySelector('select');
 const searchButton = document.querySelector('button');
 
+unitSelector.addEventListener('change', selectHandler);
+searchButton.addEventListener('click', clickHandler);
 
-(async () => {console.log('bordeaux', await logicManager.getWeatherData('bordeaux', 'metric'));})();
-(async () => {console.log('New York', await logicManager.getWeatherData('new York', 'metric'));})();
+
+const defaultCity = 'New York';  
+let currentCity;
+
+
+function clickHandler() {
+    if (cityInput.value !== '') {
+        currentCity = cityInput.value;
+        getWeather(cityInput.value);
+    } else {
+        getWeather();
+    }
+}
+
+
+function selectHandler() {
+    getWeather(currentCity);
+}
+
+
+async function getWeather(location = defaultCity, unit = unitSelector.value) {
+    console.log(location, await logicManager.getWeatherData(location, unitSelector.value));
+}
+
+
+// (async () => {console.log('bordeaux', await logicManager.getWeatherData('bordeaux', 'metric'));})();
+// (async () => {console.log('New York', await logicManager.getWeatherData('new York', 'metric'));})();
