6da53ba21d58674d679834ce6b6c88eb58859d4b
Mathboss42 <115639944+Mathboss42@users.noreply.github.com>
2023-02-08 17:57:22 +0100

    Set up processing of data received through API call

--- src/index.js
+++ src/index.js
@@ -2,5 +2,11 @@ import './styles.css';
 import * as logicManager from './logic-manager';
 import * as domManager from './dom-manager';
 
-(async () => {console.log('Rome', await logicManager.getWeather('RoMe', 'metric'));})();
-(async () => {console.log('San Marino', await logicManager.getWeather('New York', 'metric'));})();
+
+const cityInput = document.querySelector('input[type=text]');
+const unitSelector = document.querySelector('select');
+const searchButton = document.querySelector('button');
+
+
+(async () => {console.log('bordeaux', await logicManager.getWeatherData('bordeaux', 'metric'));})();
+(async () => {console.log('New York', await logicManager.getWeatherData('new York', 'metric'));})();
--- src/logic-manager.js
+++ src/logic-manager.js
@@ -1,11 +1,76 @@
-export async function getWeather(location, unit) {
-    const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}&units=${unit}`;
-    console.log(location, 'url', url);
+import { format } from "date-fns";
 
-    const data = await fetch(url);
-    console.log(location, 'data', data);
-    const result = await data.json();
-    console.log(location, 'result', result);
 
-    return result;
+async function hitAPI(location, unit) {
+    try {
+        const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&appid=${process.env.API_KEY}&units=${unit}`;
+        console.log(location, 'url', url);
+
+        const data = await fetch(url);
+        console.log(location, 'data', data);
+        const result = await data.json();
+        console.log(location, 'result', result);
+
+        return result;
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+
+function processWeatherData(dataObject) {
+    const processedData = {
+        city: dataObject.name,
+        country: convertCountry(dataObject.sys.country),
+        time: convertTime(dataObject.timezone),
+        weather: {
+            description: matchWeatherDesc(dataObject.weather[0].description),
+            temperature: dataObject.main.temp,
+            humidity: dataObject.main.humidity,
+            wind: dataObject.wind.speed,
+        }
+    };
+    
+    return processedData;
+}
+
+
+function convertTime(offset) {
+    const localTime = new Date((new Date().getTime())+offset*1000);
+    return format(localTime, 'eeee p');
+}
+
+
+function convertCountry(countryCode) {
+    const regionNames = new Intl.DisplayNames(['en'], {type: 'region'});
+    return regionNames.of(countryCode); 
+}
+
+function matchWeatherDesc(desc) {
+    switch (desc) {
+        case 'clear sky':
+        case 'few clouds':
+            return 'Sunny';
+        case 'scattered clouds':
+        case 'broken clouds':
+            return 'Cloudy';
+        case 'shower rain':
+            return 'Showers';
+        case 'rain':
+            return 'Rainy';
+        case 'thunderstorm':
+            return 'Thunderstorm';
+        case 'snow':
+            return 'Snowy';
+        case 'mist':
+            return 'Misty';
+    }
+}
+
+
+export async function getWeatherData(location, unit) {
+    const rawData = await hitAPI(location, unit);
+    const processedData = processWeatherData(rawData);
+
+    return processedData;
 }
